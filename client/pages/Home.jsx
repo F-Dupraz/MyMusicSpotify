ccad0c42ab17d227fd7b83cfe1b56e575af78194
F-Dupraz <duprazfabricio@gmail.com>
2023-04-10 09:30:07 -0300

    comienzo del frontend

---
+++ client/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Header from '../components/Header';
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+
+      <div>
+        <h2>My Songs</h2>
+      </div>
+    </>
+  );
+}
+
+export default Home;
--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ const createApp = require('./app');
 
 const app = createApp();
 
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, "../client/public", "build", "index.html")
+  );
+});
+
 app.listen(process.env.SERVER_PORT, () => {
   console.log('  Listen on port ', process.env.SERVER_PORT);
 });
