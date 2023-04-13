950e0c65d18f6ca55faa4b9501a6e1bd2fffefb2
F-Dupraz <duprazfabricio@gmail.com>
2023-04-13 07:32:53 -0300

    Finalizacion del proyecto backend con tests

--- server/index.js
+++ server/index.js
@@ -2,12 +2,6 @@ const createApp = require('./app');
 
 const app = createApp();
 
-app.get("*", (req, res) => {
-  res.sendFile(
-    path.resolve(__dirname, "../client/public", "build", "index.html")
-  );
-});
-
 app.listen(process.env.SERVER_PORT, () => {
   console.log('  Listen on port ', process.env.SERVER_PORT);
 });
